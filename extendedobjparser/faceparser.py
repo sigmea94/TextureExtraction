4d869d986f12301db63977b48465c8ce59e627bc
Stefan Schiechel <sts1757@thi.de>
2018-11-19 10:21:45 +0100

    some small adjustments on structure of classes

--- extendedobjparser/faceparser.py
+++ extendedobjparser/faceparser.py
@@ -11,7 +11,7 @@ class FaceParser(object):
         self.meshes = []    # list of faces per mesh
 
     def parse(self):
-        lines = line_generator(self.file_name, self.encoding)
+        lines = self.__line_generator(self.file_name, self.encoding)
         for line in lines:
             split = line.split()
             if len(split) < 2:
@@ -29,49 +29,49 @@ class FaceParser(object):
                     # start new faces for unnamed object
                     self.faces = []
                     self.meshes.append(self.faces)
-                faces = parse_line(line)
+                faces = self.__parse_line(line)
                 for face in faces:
                     self.faces.append(face)
         return self.meshes
 
-
-# generate triangulated faces for given line
-# raises ValueError when any vertex doesn't have texture coordinates or normals
-def parse_line(line):
-    faces = []   # list of triangulated faces: face consists of multiple vertices
-    first_vertex, prev_vertex, current_vertex = None, None, None
-    for i, vertex in enumerate(line.split()[1:]):   # each vertex is separated by a space
-        has_texture = False
-        has_normal = False
-        parts = vertex.split('/')   # each element of a vertex is separated by a slash
-        if len(parts) == 2:         # two parts: v/vt
-            has_texture = True
-        elif len(parts) == 3:       # three parts: v//vn or v/vt/vn
-            if parts[1] != '':
+    # generate triangulated faces for given line
+    # raises ValueError when any vertex doesn't have texture coordinates or normals
+    @staticmethod
+    def __parse_line(line):
+        faces = []   # list of triangulated faces: face consists of multiple vertices
+        first_vertex, prev_vertex, current_vertex = None, None, None
+        for i, vertex in enumerate(line.split()[1:]):   # each vertex is separated by a space
+            has_texture = False
+            has_normal = False
+            parts = vertex.split('/')   # each element of a vertex is separated by a slash
+            if len(parts) == 2:         # two parts: v/vt
                 has_texture = True
-            has_normal = True
-        if not has_texture or not has_normal:
-            raise ValueError("Vertices of faces should have texture coordinates and normals")
+            elif len(parts) == 3:       # three parts: v//vn or v/vt/vn
+                if parts[1] != '':
+                    has_texture = True
+                has_normal = True
+            if not has_texture or not has_normal:
+                raise ValueError("Vertices of faces should have texture coordinates and normals")
 
-        v_idx = int(parts[0]) - 1
-        vt_idx = int(parts[1]) - 1 if has_texture else None
-        vn_idx = int(parts[2]) - 1 if has_normal else None
-        prev_vertex = current_vertex
-        current_vertex = Vertex(v_idx, vt_idx, vn_idx)
-        if i == 0:
-            # save first vertex for triangulation
-            first_vertex = current_vertex
-        if i == 2:
-            # first triangle
-            faces.append([first_vertex, prev_vertex, current_vertex])
-        if i > 2:
-            # triangulate further triangles
-            faces.append([prev_vertex, current_vertex, first_vertex])
-    return faces
+            v_idx = int(parts[0]) - 1
+            vt_idx = int(parts[1]) - 1 if has_texture else None
+            vn_idx = int(parts[2]) - 1 if has_normal else None
+            prev_vertex = current_vertex
+            current_vertex = Vertex(v_idx, vt_idx, vn_idx)
+            if i == 0:
+                # save first vertex for triangulation
+                first_vertex = current_vertex
+            if i == 2:
+                # first triangle
+                faces.append([first_vertex, prev_vertex, current_vertex])
+            if i > 2:
+                # triangulate further triangles
+                faces.append([prev_vertex, current_vertex, first_vertex])
+        return faces
 
-
-def line_generator(file_name, encoding):
-    file = open(file_name, mode='r', encoding=encoding)
-    for line in file:
-        yield line
-    file.close()
+    @staticmethod
+    def __line_generator(file_name, encoding):
+        file = open(file_name, mode='r', encoding=encoding)
+        for line in file:
+            yield line
+        file.close()
--- textureextractor/extractor.py
+++ textureextractor/extractor.py
@@ -6,71 +6,71 @@ from extendedobjparser.parser import Parser
 class Extractor:
 
     def __init__(self, obj_file, camera_file, image_file, base_file=None):
-        self.obj_file = read_obj(obj_file)
-        self.camera_file = read_camera(camera_file)
-        self.image_file = read_image(image_file)
-        self.base_file = read_base(base_file)
+        self.obj_file = self.__read_obj(obj_file)
+        self.camera_file = self.__read_camera(camera_file)
+        self.image_file = self.__read_image(image_file)
+        self.base_file = self.__read_base(base_file)
 
     def extract(self):
         # Todo: extract
         # Todo: save image
         pass
 
+    @staticmethod
+    def __read_obj(obj_path):
+        # parse file, check file is obj file
+        if not obj_path.endswith(".obj"):
+            raise ValueError("model file should be an obj file")
+        # use extended obj parser
+        parser = Parser(obj_path)
+        obj = parser.parse()
+        # return scene (list of meshes)
+        return obj
 
-def read_obj(obj_path):
-    # parse file, check file is obj file
-    if not obj_path.endswith(".obj"):
-        raise ValueError("model file should be an obj file")
-    # use extended obj parser
-    parser = Parser(obj_path)
-    obj = parser.parse()
-    # return scene (list of meshes)
-    return obj
+    @staticmethod
+    def __read_camera(camera_path):
+        # returns dictionary with camera parameters
+        # camera parameters are:
+        #   - focal length
+        #   - fov
+        #   - position
+        #   - look_direction
+        if not camera_path.endswith(".json"):
+            raise ValueError("camera file should be a json file")
 
+        with open(camera_path, 'r') as f:
+            camera = json.load(f)
 
-def read_camera(camera_path):
-    # returns dictionary with camera parameters
-    # camera parameters are:
-    #   - focal length
-    #   - fov
-    #   - position
-    #   - look_direction
-    if not camera_path.endswith(".json"):
-        raise ValueError("camera file should be a json file")
+        # validate
+        if "focal_length" not in camera:
+            raise ValueError("camera parameter 'focal_length' should exist")
+        elif "fov" not in camera:
+            raise ValueError("camera parameter 'fov' should exist")
+        elif "position" not in camera:
+            raise ValueError("camera parameter 'position' should exist")
+        elif "look_direction" not in camera:
+            raise ValueError("camera parameter 'look_direction' should exist")
 
-    with open(camera_path, 'r') as f:
-        camera = json.load(f)
+        return camera
 
-    # validate
-    if "focal_length" not in camera:
-        raise ValueError("camera parameter 'focal_length' should exist")
-    elif "fov" not in camera:
-        raise ValueError("camera parameter 'fov' should exist")
-    elif "position" not in camera:
-        raise ValueError("camera parameter 'position' should exist")
-    elif "look_direction" not in camera:
-        raise ValueError("camera parameter 'look_direction' should exist")
+    @staticmethod
+    def __read_image(image_path):
+        # open image and return pixel accessible format
+        img = Image.open(image_path, mode='r')
+        return img
 
-    return camera
+    @staticmethod
+    def __read_base(base_file):
+        # check for existing uv-texture image which should be refined
+        # create a new one if there is no existing
+        if base_file is not None:
+            base = Image.open(base_file, mode='r')
+        else:
+            base = Image.new('RGB', (1024, 1024))
+        return base
 
-
-def read_image(image_path):
-    # open image and return pixel accessible format
-    img = Image.open(image_path, mode='r')
-    return img
-
-
-def read_base(base_file):
-    # check for existing uv-texture image which should be refined
-    # create a new one if there is no existing
-    if base_file is not None:
-        base = read_image(base_file)
-    else:
-        base = Image.new('RGB', (1024, 1024))
-    return base
-
-
-def save_image(image, image_path):
-    if not isinstance(image, Image):
-        raise ValueError("only pillow image objects can be saved")
-    image.save(image_path)
+    @staticmethod
+    def __save_image(image, image_path):
+        if not isinstance(image, Image):
+            raise ValueError("only pillow image objects can be saved")
+        image.save(image_path)
