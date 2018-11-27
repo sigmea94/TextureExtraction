aac3c94d6e3e64743f81eeee856cdb0ffc385e46
Stefan Schiechel <sts1757@thi.de>
2018-11-27 19:47:11 +0100

    restructured code for occlusion culling

--- textureextractor/culler.py
+++ textureextractor/culler.py
@@ -60,114 +60,102 @@ def cull_occluded(scene):
 
     :param scene: scene from which occluded faces should be removed
     """
-    # project every face to z buffer compare and discard if z value is larger
-
     # this values effect the performance: higher resolution slows down the application but increases the correctness of
-    # the z buffer. Close occlusion needs a higher resolution.
-    # with a higher threshold the resolution can be reduced. The threshold depends on the model (distances between
-    # occluded faces)
-    buffer_width = 512
-    buffer_height = 512
+    # the z buffer. Scenes with close occluding faces need a higher resolution.
+    buffer_width = 256
+    buffer_height = 256
     # threshold to prevent self occlusion resulting from discrete steps in depth buffer.
-    threshold = 0.5
+    # with a higher threshold the resolution can be reduced. The best threshold depends on the model (distances between
+    # occluded faces)
+    threshold = 0.1
 
-    buffer = __calculate_buffer(scene, buffer_width, buffer_height)
-
-    # now start occlusion culling
-    m_screen = np.zeros((4, 4))
-    m_screen[0][0] = buffer_width / 2
-    m_screen[1][1] = buffer_height / 2
-    m_screen[0][3] = buffer_width / 2
-    m_screen[1][3] = buffer_height / 2
-    m_screen[3][3] = 1
+    # calculate the position of each vertex on the buffer
+    buffer_vertices = __calculate_buffer_pos(scene, buffer_width, buffer_height)
+    # calculate the depth buffer
+    buffer = __calculate_buffer(scene, buffer_vertices, buffer_width, buffer_height)
 
     faces_to_discard = []
-    for face in scene.faces:
+    for i, face in enumerate(scene.faces):
         is_occluded = False
-        for v in face.vertices:
-            screen_pos = np.matmul(m_screen, np.append(np.array(v.pos), np.array([1])))
-            x = math.floor(screen_pos[0])
-            y = math.floor(screen_pos[1])
-            if buffer[y][x] < abs(v.pos[2]) - threshold:
+        for j in range(len(face.vertices)):
+            # buffer_pos_idx = 3 * face_idx + vertex_idx (see __calculate_screen_pos)
+            buffer_pos = buffer_vertices[i*3+j]
+            column = math.floor(buffer_pos[0])
+            row = math.floor(buffer_pos[1])
+            if buffer[row][column] < abs(buffer_pos[2]) - threshold:
                 # if there is only one vertex occluded: discard whole face
                 is_occluded = True
                 break
         if is_occluded:
             __remove_face_from_vertices(scene, face)
             faces_to_discard.append(face)
+
     for f in faces_to_discard:
         scene.faces.remove(f)
 
 
-def __calculate_buffer(scene, buffer_width, buffer_height):
+def __calculate_buffer(scene, buffer_vertices, buffer_width, buffer_height):
     """
     calculates a depth buffer for a given scene
 
-    :param scene:
-    :param buffer_width:
-    :param buffer_height:
-    :return:
+    :param scene: scene for which the buffer should be calculated
+    :param buffer_width: width of the buffer
+    :param buffer_height: height of the buffer
+    :return: the calculated depth buffer
     """
     # init buffer with max distance
     buffer = [[sys.maxsize for x in range(buffer_width)] for y in range(buffer_height)]
 
-    m_screen = np.zeros((4, 4))
-    m_screen[0][0] = buffer_width / 2
-    m_screen[1][1] = buffer_height / 2
-    m_screen[0][3] = buffer_width / 2
-    m_screen[1][3] = buffer_height / 2
-    m_screen[3][3] = 1
+    for i in range(len(scene.faces)):
+        # buffer_vertex_idx = 3 * face_idx + vertex_idx (see __calculate_screen_pos)
+        v0 = buffer_vertices[3 * i]
+        v1 = buffer_vertices[3 * i + 1]
+        v2 = buffer_vertices[3 * i + 2]
 
-    for f in scene.faces:
-        vertices_on_buffer = []
-        for v in f.vertices:
-            buffer_pos = np.matmul(m_screen, np.append(np.array(v.pos), np.array([1])))
-            x = buffer_pos[0]
-            y = buffer_pos[1]
-            z = abs(v.pos[2])
-            vertices_on_buffer.append([x, y, z])
+        # calculate the best step size in alpha and beta direction
+        # use median line for alpha
+        middle_x = 0.5 * v1[0] + 0.5 * v2[0]
+        middle_y = 0.5 * v1[1] + 0.5 * v2[1]
 
-        # use median line
-        middle_x = 0.5 * vertices_on_buffer[1][0] + 0.5 * vertices_on_buffer[2][0]
-        middle_y = 0.5 * vertices_on_buffer[1][1] + 0.5 * vertices_on_buffer[2][1]
-
-        alpha_distance = math.sqrt(
-            (vertices_on_buffer[0][0] - middle_x) ** 2 + (vertices_on_buffer[0][1] - middle_y) ** 2
-        )
+        # alpha distance in pixel
+        alpha_distance = round(math.sqrt((v0[0] - middle_x) ** 2 + (v0[1] - middle_y) ** 2))
 
         # or take biggest_distance?
-        # dist12 = math.sqrt((vertices_on_buffer[0][0] - vertices_on_buffer[1][0]) ** 2
-        #                    + (vertices_on_buffer[0][1] - vertices_on_buffer[1][1]) ** 2)
-        # dist13 = math.sqrt((vertices_on_buffer[0][0] - vertices_on_buffer[2][0]) ** 2
-        #                    + (vertices_on_buffer[0][1] - vertices_on_buffer[2][1]) ** 2)
+        # dist12 = math.sqrt((v0[0] - v1[0]) ** 2 + (v0[1] - v1[1]) ** 2)
+        # dist13 = math.sqrt((v0[0] - v2[0]) ** 2 + (v0[1] - v2[1]) ** 2)
         # alpha_distance = dist12 if dist12 > dist13 else dist13
 
-        # beta distance
-        beta_distance = math.sqrt(
-            (vertices_on_buffer[1][0] - vertices_on_buffer[2][0]) ** 2
-            + (vertices_on_buffer[1][1] - vertices_on_buffer[2][1]) ** 2
-        )
-        alpha_step = round(1 / round(alpha_distance), 5)
-        beta_step = round(1 / round(beta_distance), 5)
+        # beta distance in pixel
+        beta_distance = round(math.sqrt((v1[0] - v2[0]) ** 2 + (v1[1] - v2[1]) ** 2))
 
+        # for every pixel one step should be taken
+        # round to 5 decimal digits to prevent float incorrectness
+        alpha_step = round(1 / alpha_distance, 5)
+        beta_step = round(1 / beta_distance, 5)
+
+        # iterate over alle pixel inside the triangle using baryzentric interpolation
+        # starting on the v1 v2 edge.
         alpha = 0
         while alpha <= 1:
             beta = 0
             while beta <= 1 - alpha:
+                # alpha + beta + gamma = 1
                 gamma = round(1 - alpha - beta, 5)
-                x = alpha * vertices_on_buffer[0][0] + beta * vertices_on_buffer[1][0] \
-                    + gamma * vertices_on_buffer[2][0]
-                y = alpha * vertices_on_buffer[0][1] + beta * vertices_on_buffer[1][1] \
-                    + gamma * vertices_on_buffer[2][1]
-                z = alpha * vertices_on_buffer[0][2] + beta * vertices_on_buffer[1][2] \
-                    + gamma * vertices_on_buffer[2][2]
 
+                # interpolate
+                x = alpha * v0[0] + beta * v1[0] + gamma * v2[0]
+                y = alpha * v0[1] + beta * v1[1] + gamma * v2[1]
+                z = abs(alpha * v0[2] + beta * v1[2] + gamma * v2[2])
+
+                # set buffer value
                 row = math.floor(y)
                 column = math.floor(x)
                 if buffer[row][column] > z:
                     buffer[row][column] = z
 
+                # next beta value
                 beta = round(beta + beta_step, 5)
+            # next alpha value
             alpha = round(alpha + alpha_step, 5)
 
     # write buffer in file for debugging purposes
@@ -181,7 +169,43 @@ def __calculate_buffer(scene, buffer_width, buffer_height):
     return buffer
 
 
+def __calculate_buffer_pos(scene, width, height):
+    """
+    calculates a list of buffer position of each vertex, keeps the corresponding z value
+    the j-th vertex of the i-th face will be stored at index 3*i+j
+
+    :param scene: scene for whose points the positions are to be calculated
+    :param width: width of the buffer
+    :param height: height of the buffer
+    :return: a list of buffer positions
+    """
+    # projection matrix
+    m_buffer = np.zeros((4, 4))
+    m_buffer[0][0] = width / 2
+    m_buffer[1][1] = height / 2
+    m_buffer[0][3] = width / 2
+    m_buffer[1][3] = height / 2
+    m_buffer[2][2] = 1
+    m_buffer[3][3] = 1
+
+    buffer_vertices = []
+    for face in scene.faces:
+        for v in face.vertices:
+            screen_pos = np.matmul(m_buffer, np.append(np.array(v.pos), np.array([1])))
+            # remove w value
+            buffer_vertices.append(np.delete(screen_pos, 3).tolist())
+
+    return buffer_vertices
+
+
 def __remove_face_from_vertices(scene, face):
+    """
+    removes a face reference from corresponding vertices.
+    deletes the vertex if there are no more associated faces to the vertex
+
+    :param scene: scene the face belongs to
+    :param face: face which should be removed
+    """
     for v in face.vertices:
         # remove face reference from vertex
         v.faces.remove(face)
