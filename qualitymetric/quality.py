462e80022c781851d00af858a3b8fdc2fbc1f03c
Stefan Schiechel <sts1757@thi.de>
2019-01-08 16:37:14 +0100

    changed color difference to use lab colors

--- qualitymetric/quality.py
+++ qualitymetric/quality.py
@@ -2,49 +2,69 @@ import time
 import sys
 from PIL import Image
 import math
+from skimage import io, color
 
 
 def main():
     if len(sys.argv) is 3:
-        path1 = sys.argv[1]
-        path2 = sys.argv[2]
-        ground_truth = Image.open(path1, mode='r').convert('RGB')
-        texture = Image.open(path2, mode='r').convert('RGBA')
-        if texture.size != ground_truth.size:
+        ground_truth_path = sys.argv[1]
+        texture_path = sys.argv[2]
+
+        ground_truth = io.imread(ground_truth_path)
+        if ground_truth.shape[2] == 4:
+            ground_truth = color.rgba2rgb(ground_truth)
+        ground_truth = color.rgb2lab(ground_truth)
+
+        texture = io.imread(texture_path)
+        if texture.shape[2] != 4:
+            print("texture should be rgba Format")
+            return
+
+        texture_lab = color.rgb2lab(color.rgba2rgb(texture))
+
+        if texture.shape[0] != ground_truth.shape[0] or texture.shape[1] != ground_truth.shape[1]:
             print("Images cant be compared")
             return
 
-        visual_quality = Image.new('RGBA', (texture.width, texture.height))
-        pixels_123 = visual_quality.load()
+        visual_quality_image = Image.new('RGBA', (texture.shape[1], texture.shape[0]))
+        visual_quality_pixels = visual_quality_image.load()
 
         total_distance = 0
+        total_ratio = 0
         pixels = 0
 
-        max_distance = math.sqrt(255 ** 2 + 255 ** 2 + 255 ** 2)
+        max_distance = math.sqrt(100 ** 2 + 256 ** 2 + 256 ** 2)
 
-        for y in range(texture.height):
-            for x in range(texture.width):
-                texture_pixel = texture.getpixel((x, y))
-                if texture_pixel[3] == 0:
+        for y in range(texture.shape[0]):
+            for x in range(texture.shape[1]):
+                alpha = texture[y][x][3]
+                if alpha == 0:
                     continue
-                truth_pixel = ground_truth.getpixel((x, y))
+                ground_truth_pixel = ground_truth[y][x]
+                texture_pixel = texture_lab[y][x]
 
-                r_distance = abs(texture_pixel[0] - truth_pixel[0])
-                g_distance = abs(texture_pixel[1] - truth_pixel[1])
-                b_distance = abs(texture_pixel[2] - truth_pixel[2])
+                r_distance = abs(texture_pixel[0] - ground_truth_pixel[0])
+                g_distance = abs(texture_pixel[1] - ground_truth_pixel[1])
+                b_distance = abs(texture_pixel[2] - ground_truth_pixel[2])
 
                 distance = math.sqrt(r_distance ** 2 + g_distance ** 2 + b_distance ** 2)
-                ratio = distance/max_distance
+                difference_ratio = distance/max_distance
 
-                value = math.floor(ratio*255)
-                pixels_123[x, y] = (value, 0, 0, 255)
+                if distance > 18.78:
+                    # min 5% difference color red
+                    value = 255
+                else:
+                    value = 0
+                visual_quality_pixels[x, y] = (value, 0, 0, 255)
 
+                total_ratio += difference_ratio
                 total_distance += distance
                 pixels += 1
 
         print("Total Distance: " + str(total_distance))
         print("Average Distance: " + str(total_distance/pixels))
-        visual_quality.save("visual_quality.png")
+        print("Average Ratio: " + str(100 * total_ratio/pixels) + "%")
+        visual_quality_image.save("visual_quality.png")
 
     else:
         print("please provide ground truth and texture")
